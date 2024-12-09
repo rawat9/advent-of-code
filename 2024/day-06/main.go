b9d2892d78494c1a376b3d4026f9d2343f3e2a77
anuragrawat <anurag.rawat1@ge.com>
2024-12-09 13:42:54 +0000

    solve day-06!!

--- 2024/day-06/main.go
+++ 2024/day-06/main.go
@@ -11,29 +11,30 @@ import (
 var input string
 
 func main() {
-	part2()
+	fmt.Println(part1())
+	fmt.Println(part2())
 }
 
-func part1() {
+func part1() int {
 	matrix := parseInput()
-	move(matrix, false)
+	visited := move(matrix)
+	return len(visited)
 }
 
-func part2() {
+func part2() int {
 	matrix := parseInput()
-	visited := move(matrix, false)
-	fmt.Println(visited, len(visited))
+	visited := move(matrix)
 	cycles := 0
 
 	for _, v := range visited[2:] {
 		x, y := v[0], v[1]
 		matrix = reset(&matrix)
 		matrix[x][y] = "O"
-		if move2(matrix, true) {
+		if move2(matrix) {
 			cycles++
 		}
 	}
-	fmt.Println(cycles)
+	return cycles
 }
 
 func reset(matrix *[][]string) [][]string {
@@ -48,9 +49,8 @@ func isBlocked(matrix [][]string, x, y int, part2 bool) bool {
 	return matrix[x][y] == "#"
 }
 
-func move2(matrix [][]string, part2 bool) bool {
+func move2(matrix [][]string) bool {
 	startX, startY := findStartPosition(matrix)
-	blockedX, blockedY := findBlockedPosition(matrix)
 
 	direction := "UP"
 	matrix[startX][startY] = "X"
@@ -58,18 +58,16 @@ func move2(matrix [][]string, part2 bool) bool {
 
 	i, j := startX, startY
 	prev := [2]int{i, j}
-	//var blocked [][2]int
 	blockedCount := 0
 
 	for {
-		fmt.Println(blockedCount)
-		if blockedCount >= 15 {
+		if blockedCount >= 500 {
 			return true
 		}
 		if i < 0 || i >= len(matrix) || j < 0 || j >= len(matrix[0]) {
 			return false
 		}
-		if isBlocked(matrix, i, j, part2) {
+		if isBlocked(matrix, i, j, true) {
 			switch direction {
 			case "UP":
 				direction = "RIGHT"
@@ -80,9 +78,7 @@ func move2(matrix [][]string, part2 bool) bool {
 			case "LEFT":
 				direction = "UP"
 			}
-			if blockedX == i && blockedY == j {
-				blockedCount++
-			}
+			blockedCount++
 			i = prev[0]
 			j = prev[1]
 		} else {
@@ -99,9 +95,8 @@ func move2(matrix [][]string, part2 bool) bool {
 	}
 }
 
-func move(matrix [][]string, part2 bool) [][2]int {
+func move(matrix [][]string) [][2]int {
 	startX, startY := findStartPosition(matrix)
-	blockedX, blockedY := findBlockedPosition(matrix)
 
 	direction := "UP"
 	matrix[startX][startY] = "X"
@@ -109,16 +104,12 @@ func move(matrix [][]string, part2 bool) [][2]int {
 
 	i, j := startX, startY
 	prev := [2]int{i, j}
-	//var blocked [][2]int
-	blockedCount := 0
 
 	for {
-		if blockedCount > 10 {
-		}
 		if i < 0 || i >= len(matrix) || j < 0 || j >= len(matrix[0]) {
 			return visited
 		}
-		if isBlocked(matrix, i, j, part2) {
+		if isBlocked(matrix, i, j, false) {
 			switch direction {
 			case "UP":
 				direction = "RIGHT"
@@ -129,15 +120,6 @@ func move(matrix [][]string, part2 bool) [][2]int {
 			case "LEFT":
 				direction = "UP"
 			}
-			if blockedX == i && blockedY == j {
-				blockedCount++
-			}
-			//
-			//b := [2]int{blockedX, blockedY}
-			//if slices.Contains(blocked, b) {
-			//	fmt.Println("CYCLE", i, j)
-			//	return [][2]int{{1, 2}}
-			//}
 			i = prev[0]
 			j = prev[1]
 		} else {
@@ -147,7 +129,6 @@ func move(matrix [][]string, part2 bool) [][2]int {
 				matrix[i][j] = "X"
 			}
 		}
-		//pprint(matrix)
 
 		prev = [2]int{i, j}
 		i += DIRECTIONS[direction][0]
@@ -159,18 +140,6 @@ func findStartPosition(matrix [][]string) (int, int) {
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix[0]); j++ {
 			if matrix[i][j] == "^" {
-				return i, j
-			}
-		}
-	}
-
-	return -1, -1
-}
-
-func findBlockedPosition(matrix [][]string) (int, int) {
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[0]); j++ {
-			if matrix[i][j] == "O" {
 				return i, j
 			}
 		}
